a3a37b8d5c15c0e1414cb1fcf938ea48d373f6a8
kadeem254 <kadeemalexander254@gmail.com>
2024-04-04 20:37:14 +0300

    ADDED KEYS FILE
    Keys file manages asset keys that will be reused in various parts of the code.
    By adding them to a single file it decreases chances of spelling errors and makes changing
    keys used in multiple areas easier.

---
+++ src/Keys.js
@@ -0,0 +1,5 @@
+// SCENE KEYS
+export const SCENE_LOADING = "LoadingScene";
+export const SCENE_MAIN_MENU = "MainMenuScene";
+export const SCENE_STAGE = "StageScene";
+export const SCENE_STAGE_OVERLAY = "StageOverlayScene";
